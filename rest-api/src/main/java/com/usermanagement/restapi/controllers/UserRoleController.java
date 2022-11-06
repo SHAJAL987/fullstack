eeeed526a15613f111912587f10dc0ca43288977
SHAJAL987 <ashiqulislamshajal.cse@gmail.com>
2022-11-06 16:58:01 +0600

    test

--- rest-api/src/main/java/com/usermanagement/restapi/controllers/UserRoleController.java
+++ rest-api/src/main/java/com/usermanagement/restapi/controllers/UserRoleController.java
@@ -4,6 +4,7 @@ import javax.validation.Valid;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -11,6 +12,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.usermanagement.restapi.payloads.UserRoleDto;
 import com.usermanagement.restapi.services.UserRoleService;
+import java.util.List;
 
 @RestController
 @RequestMapping("/user/ur/v1.0")
@@ -26,6 +28,12 @@ public class UserRoleController {
     @PostMapping("/createUserRole")
     public ResponseEntity<UserRoleDto> createUserRoles(@Valid @RequestBody UserRoleDto userRoleDto){
         return new ResponseEntity<>(userRoleService.createUserRole(userRoleDto),HttpStatus.CREATED);
+    }
+
+    //Get All User Role START -------------------------->
+    @GetMapping("/findAllRole")
+    public List<UserRoleDto> getAllUserRole(){
+        return userRoleService.getAllUserRoles();
     }
 
     
