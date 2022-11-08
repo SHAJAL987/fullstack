d1cc193b248a5968de990c1e73cd43cc94c4edd7
SHAJAL987 <ashiqulislamshajal.cse@gmail.com>
2022-11-08 22:24:19 +0600

    feat: user role added

--- rest-api/src/main/java/com/usermanagement/restapi/controllers/UserRoleController.java
+++ rest-api/src/main/java/com/usermanagement/restapi/controllers/UserRoleController.java
@@ -5,7 +5,9 @@ import javax.validation.Valid;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -34,6 +36,18 @@ public class UserRoleController {
     @GetMapping("/findAllRole")
     public List<UserRoleDto> getAllUserRole(){
         return userRoleService.getAllUserRoles();
+    }
+
+    //Update User Role By Id START --------------------->
+    @PutMapping("/updateUserRole/{id}")
+    public ResponseEntity<UserRoleDto> updateUserRole(@RequestBody UserRoleDto userRoleDto, @PathVariable(name = "id") long id){
+        UserRoleDto userRoleResponse = userRoleService.updateUserRole(userRoleDto, id);
+        return new ResponseEntity<>(userRoleResponse,HttpStatus.OK);
+    }
+
+    //Delete User By Id START ------------------->
+    public ResponseEntity<String> deleteUserRoleById(@PathVariable(name = "id") long id){
+        return new ResponseEntity<>("User Role Deleted Successfully",HttpStatus.OK);
     }
 
     
